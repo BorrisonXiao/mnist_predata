3917b55299104f7baefc1bde9c478975de561b27
Cihan Xiao <borrison@Cihans-MacBook-Pro.local>
2020-03-16 14:08:07 +0800

    Added HDF5 file

--- prep_data.py
+++ prep_data.py
@@ -1,11 +1,20 @@
+# Author: Cihan Xiao
+# Email: borrisonxiao@gmail.com
+
 import numpy as np
+import h5py
 from PIL import Image
 from os import path
+import os
 
 
-def parse_labels(fname, mn, num):
+def parse_labels(fname, ftype, mn, num):
     """
     Parse labels from the original binary file.
+    :param fname: The original binary file.
+    :param ftype: The type of data, either test or training.
+    :param mn: The magic number for validation.
+    :param num: The number of elements for validation.
     :return: An array of labels.
     """
     labels_file = open(fname, 'r')
@@ -23,11 +32,11 @@ def parse_labels(fname, mn, num):
     return labels
 
 
-def parse_images(fname, type, mn, num, nr=28, nc=28):
+def parse_images(fname, ftype, mn, num, nr=28, nc=28):
     """
     Parse images from the original binary file.
     :param fname: The original binary file.
-    :param type: The type of data, either test or training.
+    :param ftype: The type of data, either test or training.
     :param mn: The magic number for validation.
     :param num: The number of elements for validation.
     :param nr: The number of rows for validation, 28 by default.
@@ -50,8 +59,8 @@ def parse_images(fname, type, mn, num, nr=28, nc=28):
 
     # Save test/training images to disk
     for i in range(N):
-        save_path = "img/" + type + "/im_" + str(i) + ".jpeg"
-        if not path.exists(save_path):
+        save_path = "img/" + ftype + "/im_" + str(i) + ".jpeg"
+        if not path.exists(save_path):  # Check if the file exists already
             im = Image.fromarray(images[i])
             im.save(save_path)
 
@@ -65,19 +74,30 @@ def main():
     Pre-process the test and training data from the original binary files and store them into a HDF5 file.
     :return: None
     """
-    training_labels = parse_labels('data/train-labels.idx1-ubyte', mn=2049, num=60000)
+    training_labels = parse_labels('data/train-labels.idx1-ubyte', ftype="training", mn=2049, num=60000)
     print(training_labels)
     print(len(training_labels))
 
-    test_labels = parse_labels('data/t10k-labels.idx1-ubyte', mn=2049, num=10000)
+    test_labels = parse_labels('data/t10k-labels.idx1-ubyte',ftype="test", mn=2049, num=10000)
     print(test_labels)
     print(len(test_labels))
 
-    training_images = parse_images('data/train-images.idx3-ubyte', type="training", mn=2051, num=60000)
+    training_images = parse_images('data/train-images.idx3-ubyte', ftype="training", mn=2051, num=60000)
     print(np.shape(training_images))
 
-    test_images = parse_images('data/t10k-images.idx3-ubyte', type="test", mn=2051, num=10000)
+    test_images = parse_images('data/t10k-images.idx3-ubyte', ftype="test", mn=2051, num=10000)
     print(np.shape(test_images))
+
+    if path.exists("mnist.h5"):  # Overwrite the file if it exists
+        os.remove("mnist.h5")
+
+    # Write all data into a HDF5 file
+    f = h5py.File("mnist.h5", "w")
+    f.create_dataset('training_labels', data=training_labels)
+    f.create_dataset('training_images', data=training_images)
+    f.create_dataset('test_labels', data=test_labels)
+    f.create_dataset('test_images', data=test_images)
+    f.close()
 
 
 if __name__ == '__main__':
